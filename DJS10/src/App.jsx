24ab532d65c851aa15dae2db9631b929674ce48e
Mathew Ritchie <ritchmat89@gmail.com>
2025-04-08 16:37:08 +0200

    have added a useStae from the errors and added a catch for error handling. Added return for the blog information.

--- DJS10/src/App.jsx
+++ DJS10/src/App.jsx
@@ -3,12 +3,40 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  const [blogs, setBlogs] = useState(0);
+  const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error fetching data: ${res.status}`);
+        }
+        return res.json();
+      })
+
+      .then((data) => {
+        setBlogs(data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error("error fetching data", error);
+        setError(error);
+        setBlogs(null);
+      });
   }, []);
   console.log(blogs);
-  return <></>;
+  console.log(error);
+
+  return (
+    <div>
+      {blogs && <h1>Posts</h1>}
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
